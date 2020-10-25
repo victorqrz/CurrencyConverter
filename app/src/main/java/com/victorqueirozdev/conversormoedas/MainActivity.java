23e9d9973c062fa8182701d9e4e7b1355b197c20
Victor <victoorqrz@gmail.com>
2020-10-25 19:01:14 -0300

    adicionando padrao viewholder

--- app/src/main/java/com/victorqueirozdev/conversormoedas/MainActivity.java
+++ app/src/main/java/com/victorqueirozdev/conversormoedas/MainActivity.java
@@ -3,12 +3,30 @@ package com.victorqueirozdev.conversormoedas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewHolder mViewHolder = new ViewHolder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.mViewHolder.edit_value = findViewById(R.id.edit_value);
+        this.mViewHolder.textDolar = findViewById(R.id.text_dolar);
+        this.mViewHolder.textEuro = findViewById(R.id.text_euro);
+        this.mViewHolder.buttonCalculate = findViewById(R.id.button_calculate);
+
+    }
+
+    private static class ViewHolder{
+        EditText edit_value;
+        TextView textDolar;
+        TextView textEuro;
+        Button buttonCalculate;
     }
 }
