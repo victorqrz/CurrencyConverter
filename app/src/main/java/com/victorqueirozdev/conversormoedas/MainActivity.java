c80e6deebb30f3af8a12d0549d261fbf535ff36f
Victor <victoorqrz@gmail.com>
2020-10-26 17:17:06 -0300

    parte lógica finalizada

--- app/src/main/java/com/victorqueirozdev/conversormoedas/MainActivity.java
+++ app/src/main/java/com/victorqueirozdev/conversormoedas/MainActivity.java
@@ -3,11 +3,13 @@ package com.victorqueirozdev.conversormoedas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private ViewHolder mViewHolder = new ViewHolder();
 
@@ -15,14 +17,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         this.mViewHolder.edit_value = findViewById(R.id.edit_value);
         this.mViewHolder.textDolar = findViewById(R.id.text_dolar);
         this.mViewHolder.textEuro = findViewById(R.id.text_euro);
         this.mViewHolder.buttonCalculate = findViewById(R.id.button_calculate);
 
+        this.mViewHolder.buttonCalculate.setOnClickListener(this);
+        this.clearValues();
     }
 
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.button_calculate){
+            String value = this.mViewHolder.edit_value.getText().toString();
+            if("".equals(value)){
+                //msg de erro
+                Toast.makeText(this, this.getString(R.string.informe_valor), Toast.LENGTH_LONG).show();
+            } else {
+                Double real = Double.valueOf(value);
+                this.mViewHolder.textDolar.setText(String.format("%.2f", real / 5.62));
+                this.mViewHolder.textEuro.setText(String.format("%.2f", real/ 6.63));
+            }
+        }
+    }
+
+    private void clearValues() {
+        this.mViewHolder.textDolar.setText("");
+        this.mViewHolder.textEuro.setText("");
+    }
     private static class ViewHolder{
         EditText edit_value;
         TextView textDolar;
